f00a4b5e28b7945fd1cac5e3eece12934361664c
ObjetivoCesar <objetivo.cesar@gmail.com>
2025-05-23 23:58:26 -0500

    Permitir CORS para pruebas externas (CodePen)

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ const upload = multer({
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
